54530ed674c2f66f8a0e992896e29d67b932b1dd
William <williammferreira16@gmail.com>
2021-08-01 10:28:22 -0400

    namespace created for app client
    The app client was given a namespace and the names of the views were changed.  The files that have redirects to this app were also changes accordingly.

--- project/chat/client/urls.py
+++ project/chat/client/urls.py
@@ -3,7 +3,9 @@ from client import views as client
 from client.views import ChatsView
 from django.contrib.staticfiles.storage import staticfiles_storage
 
+app_name = "client"
+
 urlpatterns = [
-	path('', client.main.as_view(), name="client"),
-    path('chats/<str:name>', ChatsView.as_view(), name="chats_view"),
+	path('', client.main.as_view(), name="home"),
+	path('chats/<str:name>', ChatsView.as_view(), name="detail"),
 ]
