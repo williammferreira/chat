4bff8f912bd27dfb10599453911743b646013371
William <williammferreira16@gmail.com>
2021-08-01 10:46:21 -0400

    some comments and some print/console.log statements removed
    Some comments and some print/console.log statements removed because they were not needed.

--- project/chat/client/static/js/searchSocket.js
+++ project/chat/client/static/js/searchSocket.js
@@ -6,15 +6,12 @@ searchSocket.onopen = function () {
 
 searchSocket.onmessage = function (rawData) {
   var data = JSON.parse(rawData.data);
-  // console.log(data.output[2])
-  // console.log(data.output)
   var chats = ``;
   if ((data.output).length == 0) {
     chats = `<p class='no-chats-search' style='font-size: 15px;'><i>No chats match your search.</i></p>`;
   } else {
     // 0=chatCreator 1=chatUsers 2=chatDescription 3=chatDateCreated 4=locationUrl 5=token
     for (var i = 0; i < (data.output).length; i++) {
-      console.log(data.output[i]);
       chats += `
       <div class="chat" onclick="replace('/~/chats/` + data.output[i][4] + `')">
           <div class="title"><b>` + data.output[i][0] + `, </b>` + data.output[i][1] + `</div><br>
