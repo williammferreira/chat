d138d535e2e94ad01b0fcf8efd3445b087eb546f
William <williammferreira16@gmail.com>
2021-08-29 10:02:06 -0400

    Profile model added
    The Profile model was added. NOTE: the id field is the primary key

--- project/chat/client/models.py
+++ project/chat/client/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
@@ -30,3 +32,19 @@ class messages(models.Model):
 
 	class Meta:
 		db_table = 'messages'
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	id = models.AutoField(primary_key=True);
+
+
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+	instance.profile.save()
