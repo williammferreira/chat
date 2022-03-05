502c7701e2c58efcc3afc13f4df22894b179fc6a
codeisamazing <>
2022-03-05 12:28:04 +0000

    Add docs models and migrations
    The Document and Group models were added to the docs application. The
    migrations were applied.

--- docs/models.py
+++ docs/models.py
@@ -2,3 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+class Document(models.Model):
+	name = models.CharField(max_length=50, primary_key=True)
+	slug = models.CharField(max_length=50)
+	content = models.TextField()
+	group = models.OneToOneField('Group', on_delete=models.CASCADE, null=True, blank=True, default=None)
+
+class Group(models.Model):
+	name = models.CharField(max_length=50, primary_key=True)
+	slug = models.CharField(max_length=50)
+	parent = models.OneToOneField('self', on_delete=models.CASCADE, null=True, blank=True, default=None)
