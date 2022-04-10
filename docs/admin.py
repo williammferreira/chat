f3251a264aae4925dbc48e38b5f06650b771ab7d
William Ferreira <williammferreira16@gmail.com>
2022-04-10 10:32:53 -0400

    Change document model

--- docs/admin.py
+++ docs/admin.py
@@ -4,9 +4,21 @@ from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here
 
-class GroupAndDocumentModelAdmin(SummernoteModelAdmin):
-	summernote_fields = ('content')
-	list_display = ('name', 'slug')
+@admin.register(Group)
+class GroupModelAdmin(SummernoteModelAdmin):
+	list_display = ('title', 'slug')
+	search_fields = ('title',)
+	prepopulated_fields = {'slug': ('title',)}
+	raw_id_fields = ('parent',)
+	exclude = ('id',)
 
-admin.site.register(Group, GroupAndDocumentModelAdmin)
-admin.site.register(Document, GroupAndDocumentModelAdmin)
+
+@admin.register(Document)
+class DocumentModelAdmin(SummernoteModelAdmin):
+	summernote_fields = ('body',)
+	list_filter = ('status', 'group')
+	list_display = ('title', 'slug')
+	search_fields = ('title', 'body')
+	prepopulated_fields = {'slug': ('title',)}
+	raw_id_fields = ('group',)
+	exclude = ('id',)
