e0ea942273e34e6de14eb31b90d52a8707efa702
William Ferreira <williammferreira16@gmail.com>
2022-04-18 07:37:01 -0400

    Add choices to theme field in the profile model

--- client/models.py
+++ client/models.py
@@ -8,49 +8,64 @@ import uuid
 
 # Create your models here.
 
+
 class Chats(models.Model):
-	chatCreator = models.TextField()
-	chatUsers = models.TextField()
-	chatDescription = models.TextField()
-	chatDateCreated = models.TextField()
-	locationUrl = models.TextField()
-	token = models.TextField(primary_key=True)
+    chatCreator = models.TextField()
+    chatUsers = models.TextField()
+    chatDescription = models.TextField()
+    chatDateCreated = models.TextField()
+    locationUrl = models.TextField()
+    token = models.TextField(primary_key=True)
 
-	def get_absolute_url(self):
-		return reverse("client:detail", args=[self.locationUrl])
+    def get_absolute_url(self):
+        return reverse("client:detail", args=[self.locationUrl])
 
-	class Meta:
-		db_table = 'chats'
+    class Meta:
+        db_table = 'chats'
+
 
 class messages(models.Model):
-	chat = models.ForeignKey(Chats, on_delete=models.CASCADE, db_column="chat")
-	message = models.TextField()
-	creator = models.TextField()
-	date = models.DateTimeField()
-	id = models.TextField(primary_key=True)
-	token = models.TextField()
-	
+    chat = models.ForeignKey(Chats, on_delete=models.CASCADE, db_column="chat")
+    message = models.TextField()
+    creator = models.TextField()
+    date = models.DateTimeField()
+    id = models.TextField(primary_key=True)
+    token = models.TextField()
 
-	class Meta:
-		db_table = 'messages'
+    class Meta:
+        db_table = 'messages'
+
 
 class Profile(models.Model):
-	user = models.OneToOneField(User, on_delete=models.CASCADE)
-	id = models.UUIDField(primary_key=True, editable=False, unique=True, default=uuid.uuid4)
-	theme = models.CharField(default="light", max_length=5)
+    DARK = 'dark'
+    BLACK = 'black'
+    LIGHT = 'light'
 
-	class Meta:
-		db_table = 'profile'
+    THEME_CHOICES = (
+        # Dark
+        (DARK, 'Dark'),
+        (BLACK, 'Black'),
 
+        # Light
+        (LIGHT, 'Light'),
+    )
 
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    id = models.UUIDField(primary_key=True, editable=False,
+                          unique=True, default=uuid.uuid4)
+    theme = models.CharField(choices=THEME_CHOICES,
+                             default="light", max_length=5)
 
+    class Meta:
+        db_table = 'profile'
 
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-	if created:
-		Profile.objects.create(user=instance)
+    if created:
+        Profile.objects.create(user=instance)
+
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-	instance.profile.save()
+    instance.profile.save()
