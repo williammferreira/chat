3b22018f6ed8cb3ec8f1df1ec5205b3794e33a38
William Ferreira <williammferreira16@gmail.com>
2022-05-13 17:05:58 -0400

    Add a view to list all chats

--- client/urls.py
+++ client/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from client import views as client
-from .views import ChatsView
+from . import views as client
+from .views import AllChatsListView, ChatsView
 
 app_name = "client"
 
 urlpatterns = [
 	path('', client.Main.as_view(), name="home"),
 	path('chats/<str:name>', ChatsView.as_view(), name="detail"),
+    path('chats/', AllChatsListView.as_view(), name='allchats'),
 ]
