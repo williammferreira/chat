7cab73cee5449c8328ef835038244636cfdbbf6b
William Ferreira <williammferreira16@gmail.com>
2022-06-12 11:21:45 +0000

    Add store view

--- store/views.py
+++ store/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from management.models import App
 
 # Create your views here.
+
+
+class AppListView(ListView):
+    model = App
+    template_name = "store/store.html"
