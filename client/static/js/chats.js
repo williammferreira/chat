3f418ac7f0522314dcc413a18d7e397c10fa0070
williammferreira <williammferreira16@gmail.com>
2022-07-20 10:20:35 -0400

    Add XMLHttpRequest to pin button

--- client/static/js/chats.js
+++ client/static/js/chats.js
@@ -1,19 +1,51 @@
-var chats = document.querySelectorAll('.chat')
+var chats = document.querySelectorAll(".chat");
+var pin_buttons = document.querySelectorAll(".pin-button");
 
-document.addEventListener('DOMContentLoaded', function () {
-    var divs = document.querySelectorAll('.users');
+document.addEventListener("DOMContentLoaded", function () {
+    var divs = document.querySelectorAll(".users");
     divs.forEach(function (div) {
         html = div.innerHTML;
-        html = html.replace(/ /g, '');
-        html = html.replace(/\r?\n|\r/g, '');
-        html = html.replace(new RegExp(',', 'g'), ', ');
-        console.log(html);
+        html = html.replace(/ /g, "");
+        html = html.replace(/\r?\n|\r/g, "");
+        html = html.replace(new RegExp(",", "g"), ", ");
         div.innerHTML = html;
     });
 });
 
 chats.forEach(function (chat) {
-    chat.addEventListener('click', function (evt) {
-        location.href = chat.getAttribute('data-url');
+    chat.addEventListener("click", function (evt) {
+        if (!evt.target.classList.contains("no-redirect")) {
+            location.href = chat.getAttribute("data-url");
+        }
+    });
+});
+
+pin_buttons.forEach((btn) =>
+    btn.addEventListener("click", function (evt) {
+        url = btn.getAttribute("data-url");
+        chat = btn.getAttribute("data-chat");
+        csrf = btn.getAttribute("data-csrf");
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url + "?location=" + chat, true);
+        xhr.send();
+
+        var img = btn.getElementsByTagName("img")[0];
+
+        console.log(
+            img.src +
+                "\n" +
+                window.location.origin +
+                img.getAttribute("data-pinned")
+        );
+
+        if (
+            img.src ==
+            window.location.origin + img.getAttribute("data-pinned")
+        ) {
+            img.src = img.getAttribute("data-pin");
+        } else {
+            img.src = img.getAttribute("data-pinned");
+        }
     })
-})
+);
