6b5213fd0f04cffbe63a4a5a4163e71b8bdb6c64
williammferreira <williammferreira16@gmail.com>
2022-07-20 15:12:05 -0400

    Make reject chat button functional

--- client/static/js/accept-chat.js
+++ client/static/js/accept-chat.js
@@ -9,5 +9,20 @@ accept.forEach(function (btn) {
         var xhr = new XMLHttpRequest();
         xhr.open("GET", `${url}?location=${chat}`, true);
         xhr.send();
+
+        btn.parentElement.parentElement.remove();
+    });
+});
+
+reject.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        url = btn.getAttribute("data-url");
+        chat = btn.getAttribute("data-chat");
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", `${url}?location=${chat}`, true);
+        xhr.send();
+
+        btn.parentElement.parentElement.remove();
     });
 });
