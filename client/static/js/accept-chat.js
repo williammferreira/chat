f8e320c6ec9393a939a679003b3eedbfcb3564c7
williammferreira <williammferreira16@gmail.com>
2022-07-20 11:17:52 -0400

    Make accept button work

---
+++ client/static/js/accept-chat.js
@@ -0,0 +1,13 @@
+const accept = document.querySelectorAll(".accept");
+const reject = document.querySelectorAll(".reject");
+
+accept.forEach(function (btn) {
+    btn.addEventListener("click", function () {
+        url = btn.getAttribute("data-url");
+        chat = btn.getAttribute("data-chat");
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", `${url}?location=${chat}`, true);
+        xhr.send();
+    });
+});
